18c90ee59980acff1e4796385be7914017e10034
Syd Sisco <hello@sydsis.co>
2021-02-09 03:48:35 +0000

    adds tests using M&C

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-    if (!error) callback(data);
+    // console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      callback(data);
+    } else {
+      callback();
+    }
   });
 };
 
-breedDetailsFromFile('Bombay', catData => {
-  console.log(catData);
-});
+// breedDetailsFromFile('Bombay', catData => {
+//   console.log(catData);
+// });
+
+module.exports = breedDetailsFromFile;
