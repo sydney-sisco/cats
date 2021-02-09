a574757bddaebbeded1725076b32134a089e8286
Syd Sisco <hello@sydsis.co>
2021-02-09 03:28:08 +0000

    implements an async call to read data from a file

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+breedDetailsFromFile('Bombay', catData => {
+  console.log(catData);
+});
